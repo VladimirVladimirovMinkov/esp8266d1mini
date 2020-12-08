f0974e8916aade1f1da273614179581ed24a03bb
VladimirVladimirovMinkov <51568939+VladimirVladimirovMinkov@users.noreply.github.com>
2020-12-08 21:48:55 +1300

    finished domoticz
    end of domotics changes
    setup rudimentary webserver

--- esp8266 code/webserv/webserv.ino
+++ esp8266 code/webserv/webserv.ino
@@ -1,25 +1,29 @@
-// librarys to be used
-#include <DHT.h>
+// libaries used
+#include <dummy.h>
+#include <DHTesp.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
-
-// definitions for use
-#define ONE_WIRE_PIN_SB 2
-#define ONE_WIRE_PIN_DHT 15
-#define WIFI_SSID "homem1"
-#define WIFI_PASS "Yanaminkovladkodestroytheworld1"
-#include <ESP8266WiFi.h>
 #include "Adafruit_MQTT.h"
 #include "Adafruit_MQTT_Client.h"
 
-// wifi/mqt/tcp values for connection verification
-#define AIO_SERVER      "192.168.1.241" // prfered ip
-#define AIO_SERVERPORT  1883                   // use 8883 for SSL, 1883 for mqtt/tcp to domoticz
+// ds18b20 and DHT22 one wire communication wires
+#define ONE_WIRE_PIN_DS 2 // d4
+#define ONE_WIRE_PIN_DHT 0 // d3
+
+// wifi connection
+#define WIFI_SSID "homem1"
+#define WIFI_PASS "Yanaminkovladkodestroytheworld1"
+
+// mqtt and domoticz information and connection
+#define AIO_SERVER      "192.168.1.241"
+#define AIO_SERVERPORT  1883                   // use 8883 for SSL
 #define AIO_USERNAME    "vladi"
 #define AIO_KEY         ""
 
+// set dhtesp communication to dht
+DHTesp dht;
 
 // Create an ESP8266 WiFiClient class to connect to the MQTT server.
 WiFiClient client;
@@ -28,109 +32,175 @@ WiFiClient client;
 Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
 
 // Setup a feed called sensor1 for publishing.
-// Notice MQTT paths for domoticz follow the form: domoticz/<in or out>
+// Notice MQTT paths for domoticz follows: domoticz/<in or out>
 Adafruit_MQTT_Publish sensor1 = Adafruit_MQTT_Publish(&mqtt, "domoticz/in");
 
-// Setup a feed called switch_term for subscribing to changes.
-Adafruit_MQTT_Subscribe onoffbutton = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/switch_term");
+// Setup a feed called 'onoff' for subscribing to changes.
+Adafruit_MQTT_Subscribe onoffbutton = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/switch_vlad");
 
-// initiate the webserver on which port
+// setup webserver
 ESP8266WebServer webserver(80);
-// initiate one wire communication
-OneWire oneWireSB(ONE_WIRE_PIN_SB);
-OneWire oneWireDHT(ONE_WIRE_PIN_DHT);
-// initiate sensors to communicate under one wire communication
-DallasTemperature sensor_sb(&oneWireSB);
-DallasTemperature sensor_dht(&oneWireDHT);
 
-// initiate psuedo threding
-int period = 6000;
-int time_now = 0;
+// sets up one wire communication with ds pin
+OneWire oneWire_ds(ONE_WIRE_PIN_DS);
 
-// initiate debug values for temprature
-float temp_sb = -1;
-String dis_temp = "-2";
-float temp_dht = -3;
+// setsup the ds18b20 on the onewire communication
+DallasTemperature ds_sensor(&oneWire_ds);
 
+// time keeping variables
+int period_ds = 60000;
+int time_now_ds = 0;
+int period_dht = 60000;
+int time_now_dht = 0;
 
-// connects to mqtt
+// setsup debug values that are impossible for ds_sensor used
+float temp_ds = -120;
+String dis_temp_ds = "-20";
+float h = -50;
+float t = -60;
+
 void MQTT_connect();
 
-// this is the root page that will be called when the ip is typed in
 void rootPage() {
+  // creates the home page
   String res; 
   res += "Vlad Api \n";
-  res += "/temp \n";
-  res += "/hunidity \n"; 
+  res += "/current_dht \n";
+  res += "/current_ds \n";
+  res += "/ds \n";
+  res += "/dht \n";
   webserver.send(200, "text/plain", res);
 }
 
-// this will send the cached value of temprature as a page when /temp is applyed to ip
 void readTemp() { 
-  webserver.send(200, "text/plain", dis_temp);
+  // publishes the cached temprature from ds
+  webserver.send(200, "text/plain", "temprature " + String(temp_ds));
 }
 
-// this will read current temp and send it as a page when /currenttemp is applyed to ip
-void readCurrentTemp() { 
-  sensor_dht.requestTemperatures();
-  sensor_sb.requestTemperatures();
-  temp_dht = sensor_sb.getTempCByIndex(0);
-  dis_temp = String(temp_sb);
-  dis_temp += ", ";
-  dis_temp += String(temp_dht);
-  webserver.send(200, "text/plain", dis_temp);
+void readCurrent() { 
+  // gets current temprature and humidity and publishes it to web page
+  h = dht.getHumidity();
+  t = dht.getTemperature();
+  String dis = "Temprature: " + String(t) + " Humidity: " + String(h); 
+  webserver.send(200, "text/plain", dis);
 }
 
-// when an unknown modifier is added to ip this page will be sent
-void notfoundPage(){ 
+void dht22() {
+  webserver.send(200, "text/plain", "Temprature: " + String(t) + " Humidity: " + String(h));
+}
+
+void dsCurrent() {
+  // reads ds and publishes to web page
+  ds_sensor.requestTemperatures();
+  float temp = ds_sensor.getTempCByIndex(0);
+  webserver.send(200, "text/plain", String(temp));
+}
+
+void notfoundPage(){
+  // a debug message to know that this is not a page that exsists
   webserver.send(404, "text/plain", "404: Not found"); 
 }
 
 void setup() {
-  // begin serial
+  // initialise the serrial connection
   Serial.begin(115200);
   Serial.println();
 
-  // will connect to specified router and retry every 100 miliseconds to connect if a connecion hasn't been made
+  // setup pin communication
+  dht.setup(ONE_WIRE_PIN_DHT, DHTesp::DHT22);
+
+  // sets up the wifi connection
   WiFi.begin(WIFI_SSID, WIFI_PASS);
   while (WiFi.status() != WL_CONNECTED) { delay(100); }
 
-  // will put this is serrial for debug
+  // debug messages to show wifi connection is running properly
   Serial.println(F("Adafruit MQTT demo"));
   Serial.print("Connected! IP address: ");
-  // local ip on debug (should try to get it to print external ip aswell)
   Serial.println(WiFi.localIP());
 
-  // the pages when being called
+  // web pages displayed on request
   webserver.on("/", rootPage);
-  webserver.on("/temp", readTemp);
-  webserver.on("/currenttemp", readCurrentTemp);
+  webserver.on("/ds", readTemp);
+  webserver.on("/dht", dht22);
+  webserver.on("/current_dht", readCurrent);
+  webserver.on("/current_ds", dsCurrent);
+
+  // web page not found web page
   webserver.onNotFound(notfoundPage);
+
+  // begins teh web server
   webserver.begin();
+
+  // Setup MQTT subscription for onoff feed.
+  // mqtt.subscribe(&onoffbutton);
 }
 
 void loop(void){ 
-  // when connected will sort the client
+  // begins webserver clients
   webserver.handleClient();
 
-  // will check if enough time has passed since last cached temprature
-  if (millis() - time_now > period) {
-    time_now = millis();
-    sensor_sb.requestTemperatures();
-    temp_sb = sensor_sb.getTempCByIndex(0);
+  // if enough time has passed it will send an update to domoticz from the dht22 sensor
+  if (millis() - time_now_dht > period_dht) {
+    time_now_dht = millis();
 
-    // sets up the value that is to be sent under mqtt to domoticz in a format for updating virtual sensor
-    dis_temp = "{\"idx\":42, \"nvalue\":0, \"svalue\":\"";
-    dis_temp += String(temp_sb);
-    dis_temp += "\"}";
+    // retrive temprature and humidity from dht22
+    float h = dht.getHumidity();
+    float t = dht.getTemperature();
 
-    // arrays temprature to be recognised by domoticz as virtual thermal sensor1
-    //idx is identification, nvalue is spare value, svalue is value of sensor
+    // debug hum and temp
+    Serial.print("{\"humidity\": ");
+    Serial.print(h);
+    Serial.print(", \"temp\": ");
+    Serial.print(t);
+    Serial.print("}\n");
+    
+    // format into domoticz readable format
+    String dis_temp_dht = "{\"idx\":43, \"nvalue\":0, \"svalue\":\"";
+    dis_temp_dht += String(t);
+    dis_temp_dht += ";";
+    dis_temp_dht += String(h);
+    dis_temp_dht += ";";
+    dis_temp_dht += "1";
+    dis_temp_dht += "\"}";
+
+    Serial.print(dis_temp_dht);
+
+    // turns things into a char array to be used for domoticz
+    char char_temp[50];
+    //sprintf(char_temp,"{\"idx\":43, \"nvalue\":0, \"svalue\":\"%.2f\"} ", temp);
+    dis_temp_dht.toCharArray(char_temp, 50);
+
+    // will publish the temprature and return if they have published it to location
+    if (! sensor1.publish(char_temp)) {
+      Serial.println(F("Failed"));
+    } else {
+      Serial.println(F("OK!"));
+    }
+  }
+
+  // if enough time has passed it will send an update to domoticz from the ds18b20 sensor
+  if (millis() - time_now_ds > period_ds) {
+    // updates last time the temprature is checked
+    time_now_ds = millis();
+
+    // gets current sensor values and sets the temprature under the variable temp
+    ds_sensor.requestTemperatures();
+    temp_ds = ds_sensor.getTempCByIndex(0);
+
+    Serial.println(temp_ds);
+
+    // formats the current temprature into a domoticz readable format
+    // idx 42 is identification used in place of sensor1 or themal sensor
+    dis_temp_ds = "{\"idx\":42, \"nvalue\":0, \"svalue\":\"";
+    dis_temp_ds += String(temp_ds);
+    dis_temp_ds += "\"}";
+
+    // turns things into a char array to be used for domoticz
     char char_temp[50];
     //sprintf(char_temp,"{\"idx\":42, \"nvalue\":0, \"svalue\":\"%.2f\"} ", temp);
-    dis_temp.toCharArray(char_temp, 50);
+    dis_temp_ds.toCharArray(char_temp, 50);
 
-    // will state in debug whether it was published to mqtt or nor
+    // will publish the temprature and return if they have published it to location
     if (! sensor1.publish(char_temp)) {
       Serial.println(F("Failed"));
     } else {
@@ -142,6 +212,14 @@ void loop(void){
   // connection and automatically reconnect when disconnected).  See the MQTT_connect
   // function definition further below.
   MQTT_connect();
+
+  //Adafruit_MQTT_Subscribe *subscription;
+  //while ((subscription = mqtt.readSubscription(5000))) {
+    //if (subscription == &onoffbutton) {
+      //Serial.print(F("Got: "));
+      //Serial.println((char *)onoffbutton.lastread);
+    //}
+  
 }
 
 // Function to connect and reconnect as necessary to the MQTT server.
@@ -154,13 +232,10 @@ void MQTT_connect() {
     return;
   }
 
-  // debug to know con necting
   Serial.print("Connecting to MQTT... ");
-
-  // number of times to retry
-  uint8_t retries = 3;
-
-  // will retry connecting until there are no more retrys and will require resetting
+  
+  // will retry connecting if no connection is made until a resonable amount of retrys are made
+  uint8_t retries = 5;
   while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
        Serial.println(mqtt.connectErrorString(ret));
        Serial.println("Retrying MQTT connection in 5 seconds...");
@@ -172,6 +247,5 @@ void MQTT_connect() {
          while (1);
        }
   }
-  // debug so you know it has connected
   Serial.println("MQTT Connected!");
 }
