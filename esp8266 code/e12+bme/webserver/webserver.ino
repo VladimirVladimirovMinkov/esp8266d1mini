faa12eccb292af65895b6c835a58a83b4925e7b1
VladimirVladimirovMinkov <51568939+VladimirVladimirovMinkov@users.noreply.github.com>
2021-11-05 13:17:56 +1300

    setup test website
    I've got a basic website working on the esp module, now I just need to customize it and setup other parts and eventually move away from little thing.

--- esp8266 code/e12+bme/webserver/webserver.ino
+++ esp8266 code/e12+bme/webserver/webserver.ino
@@ -1,58 +1,32 @@
-// esp wifi and webserver get
+//============================================
+//ESP8266 Web Server (HTML + CSS + JavaScript)
+//============================================
+#include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
-
-// wifi connection
-#define WIFI_SSID "homem1"
-#define WIFI_PASS "Yanaminkovladkodestroytheworld1"
-
-// setup webserver
-ESP8266WebServer webserver(80);
-
-void rootPage() {
-  // creates the home page
-  String res; 
-  res += "Vlad API \n";
-  webserver.send(200, "text/plain", res);
+#include "webpageCode.h";
+//------------------------------------------
+ESP8266WebServer server(80);
+const char* ssid = "Redmi";
+const char* password = "pass1234";
+//------------------------------------------
+void webpage()
+{
+  server.send(200,"text/html", webpageCode);
 }
-
-void rootPage2() {
-  // creates the home page
-  String res; 
-  res += "<h1>Vlad API<h1> \n";
-  webserver.send(200, "text/html", res);
-}
-
-void notfoundPage(){
-  // a debug message to know that this is not a page that exsists
-  webserver.send(404, "text/plain", "404: Not found"); 
-}
-
-void setup() {
-  // initialise the serrial connection
-  Serial.begin(9600);
+//=================================================================
+void setup()
+{
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  while(WiFi.status()!=WL_CONNECTED){delay(500);Serial.print(".");}
   Serial.println();
+  Serial.print("IP Address: "); Serial.println(WiFi.localIP());
 
-  // sets up the wifi connection
-  WiFi.begin(WIFI_SSID, WIFI_PASS);
-  while (WiFi.status() != WL_CONNECTED) { delay(100); }
-
-  // debug messages to show wifi connection is running properly
-  Serial.println(F("webserver test demo"));
-  Serial.print("Connected! IP address: ");
-  Serial.println(WiFi.localIP());
-
-  // web pages displayed on request
-  webserver.on("/", rootPage);
-  webserver.on("/alt", rootPage2);
-
-  // web page not found web page
-  webserver.onNotFound(notfoundPage);
-
-  // begins the web server
-  webserver.begin();
+  server.on("/", webpage);
+  server.begin();
 }
-
-void loop(void){
-  // begins webserver clients
-  webserver.handleClient();
+//=================================================================
+void loop()
+{
+  server.handleClient();
 }
