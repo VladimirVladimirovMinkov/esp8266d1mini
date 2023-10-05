bea4c53b94b3ad8ff0fb491e2873df7f038f790e
VladimirVladimirovMinkov <51568939+VladimirVladimirovMinkov@users.noreply.github.com>
2023-10-05 14:12:34 +1300

    add time changer
    added button and variable delay
    next: add interrupts for better control

--- esp8266 code/test_monoamp_i2s/test_monoamp_i2s.ino
+++ esp8266 code/test_monoamp_i2s/test_monoamp_i2s.ino
@@ -7,20 +7,23 @@
 #include "AudioOutputMixer.h"
 
 #include "viola.h"
+
 AudioGeneratorWAV *wav[2];
 AudioFileSourcePROGMEM *file[2];
 AudioOutputI2S *out;
 AudioOutputMixer *mixer;
 AudioOutputMixerStub *stub[2];
 
-#define D5 AmpSDpin
-
 void setup()
 {
+  pinMode(2, OUTPUT);
+  pinMode(4, INPUT_PULLUP);
+  
   WiFi.mode(WIFI_OFF); //turns wifi off for power saving
   Serial.begin(115200);
   delay(100);
-  Serial.print("WAV start\n");
+
+  digitalWrite(2, LOW);
 
   // defines viola.h for playing 
   audioLogger = &Serial;
@@ -31,24 +34,30 @@ void setup()
   stub[0]->SetGain(0.3);
   wav[0] = new AudioGeneratorWAV();
   wav[0]->begin(file[0], stub[0]);
-  Serial.print("starting 0\n");
 }
 
 void loop()
 {
   static uint32_t start = 0;
+  int sleep = 1;
   static bool go = false;
   
   if (!start) start = millis();
 
   if (wav[0]->isRunning()) {
-    if (!wav[0]->loop()) { wav[0]->stop(); stub[0]->stop(); Serial.print("stopping 0\n"); }
+    if (!wav[0]->loop()) { wav[0]->stop(); stub[0]->stop(); }
+  }
+
+  if (digitalRead(4) == 0)
+  {
+    start = millis();
+    sleep = sleep*4;
+    if (sleep > 3600) sleep = 1;
   }
 
   if (millis()-start > 3000) {
     if (!go) {
       // defines viola.h for playing 
-      Serial.printf("starting 1\n");
       stub[1] = mixer->NewInput();
       stub[1]->SetGain(0.4);
       wav[1] = new AudioGeneratorWAV();
@@ -56,18 +65,19 @@ void loop()
       wav[1]->begin(file[1], stub[1]);
       go = true;
     }
+
     if (wav[1]->isRunning()) { 
       // keeps program here until end of viola.h then stops playing
-      if (!wav[1]->loop()) { wav[1]->stop(); stub[1]->stop(); Serial.print("stopping 1\n");} 
+      if (!wav[1]->loop()) { wav[1]->stop(); stub[1]->stop();} 
 
-      // brings amplifier pin low
-      pinMode(AmpSDpin, OUTPUT); 
-      digitalWrite(AmpSDpin, LOW); 
-      delay(10); 
+      // shuts down amplifier
+      pinMode(14, OUTPUT);
+      delay(100);
+      digitalWrite(14, LOW);
+      delay(100);
 
       // restarts the esp after x amount of microseconds or 1^-6 seconds
-      ESP.deepSleep(3e6); 
+      ESP.deepSleep(sleep * 5e6); 
     }
   }
-
 }
