fa6c697643b1d82c105cf139d4d6c5ddead96254
VladimirVladimirovMinkov <51568939+VladimirVladimirovMinkov@users.noreply.github.com>
2023-10-20 10:58:22 +1300

    Clean up code
    Cleaned up code and added beeping to signify the deep sleep time

--- esp8266 code/test_monoamp_i2s/test_monoamp_i2s.ino
+++ esp8266 code/test_monoamp_i2s/test_monoamp_i2s.ino
@@ -1,5 +1,7 @@
 #include <Arduino.h>
+
 #include <ESP8266WiFi.h>
+#include <ESP_EEPROM.h>
 
 #include "AudioFileSourcePROGMEM.h"
 #include "AudioGeneratorWAV.h"
@@ -7,6 +9,7 @@
 #include "AudioOutputMixer.h"
 
 #include "viola.h"
+#include "AudioFileSourceFunction.h"
 
 AudioGeneratorWAV *wav[2];
 AudioFileSourcePROGMEM *file[2];
@@ -14,66 +17,114 @@ AudioOutputI2S *out;
 AudioOutputMixer *mixer;
 AudioOutputMixerStub *stub[2];
 
-void setup()
-{
-  pinMode(2, OUTPUT);
-  pinMode(4, INPUT_PULLUP);
-  
-  WiFi.mode(WIFI_OFF); //turns wifi off for power saving
-  Serial.begin(115200);
-  delay(100);
+int hz = 880;
 
-  digitalWrite(2, LOW);
+// pre-defined function can also be used to generate the wave
+float sine_wave(const float time) {
+  float v = sin(TWO_PI * hz * time);  // C
+  v *= fmod(time, 1);               // change linear
+  v *= 0.5;                           // scale
+  return v;
+};
 
-  // defines viola.h for playing 
+AudioGeneratorWAV *wavsin;
+AudioFileSourceFunction *filesin;
+AudioOutputI2S *outsin;
+
+int sleep = -1;
+#define btn_input 5   // D1
+#define btn_output 0  // D3
+#define amp_sd 14     //  D5
+
+void setup() {
+  Serial.begin(74880);
+  delay(50);
+  EEPROM.begin(1);
+
+  filesin = new AudioFileSourceFunction(8.);
+  filesin->addAudioGenerators([&](const float time) {
+    float v = sin(TWO_PI * hz * time);  // generate sine wave
+    v *= fmod(time, 1.f);               // change linear
+    v *= 0.5;                           // scale
+    return v;
+  });
+
+  outsin = new AudioOutputI2S();
+  wavsin = new AudioGeneratorWAV();
+
+  pinMode(btn_output, OUTPUT);
+  pinMode(btn_input, INPUT_PULLUP);
+
+  WiFi.mode(WIFI_OFF);  //turns wifi off for power saving
+
+  digitalWrite(btn_output, LOW);
+
+  // defines viola.h for playing
   audioLogger = &Serial;
-  file[0] = new AudioFileSourcePROGMEM( viola, sizeof(viola) );
+  file[0] = new AudioFileSourcePROGMEM(viola, sizeof(viola));
   out = new AudioOutputI2S();
   mixer = new AudioOutputMixer(32, out);
   stub[0] = mixer->NewInput();
   stub[0]->SetGain(0.3);
   wav[0] = new AudioGeneratorWAV();
   wav[0]->begin(file[0], stub[0]);
+
+  EEPROM.get(0, sleep);
+  delay(10);
+  Serial.println(sleep);
+  
+  if ((sleep < 1) || (sleep > 15))
+    sleep = 1;
+  
+  Serial.println(sleep);
+  delay(10);
 }
 
-void loop()
-{
-  int sleep = 1;
-  static bool go = false;
+void loop() {
+  bool go = false;
 
   if (wav[0]->isRunning()) {
-    if (!wav[0]->loop()) { wav[0]->stop(); stub[0]->stop(); }
+    go = true;
+    if (!wav[0]->loop()) {
+      wav[0]->stop();
+      stub[0]->stop();
+      go = false;
+    }
   }
 
-  if (digitalRead(4) == 0)
-  {
-    sleep = sleep*4;
-    if (sleep > 3600) sleep = 1;
+  if (digitalRead(btn_input) == 0) {
+    // wav[0]->stop();
+    // stub[0]->stop();
+    delay(10);
+    if (sleep == 1) sleep = 2;
+    else if (sleep == 2) sleep = 5;
+    else if (sleep == 5) sleep = 10;
+    else if (sleep == 10) sleep = 15;
+    else sleep = 1;
+    Serial.println(sleep);
+    EEPROM.put(0, sleep);
+    delay(10);
+    EEPROM.commit();
+
+     wavsin->begin(filesin, outsin);
+     delay(100);
+    
+    // for (int i = 0; i < sleep*2; i++)
+    // {
+    //   wavsin->begin(filesin, outsin);
+    //   delay(100);
+    // }
+    wavsin->stop();
   }
 
-  if (millis() > 5000) {
-    if (!go) {
-      // defines viola.h for playing 
-      stub[1] = mixer->NewInput();
-      stub[1]->SetGain(0.4);
-      wav[1] = new AudioGeneratorWAV();
-      file[1] = new AudioFileSourcePROGMEM( viola, sizeof(viola) );
-      wav[1]->begin(file[1], stub[1]);
-      go = true;
-    }
+  if ((millis() > 5000) || (go == false)) {
+    // shuts down amplifier
+    pinMode(amp_sd, OUTPUT);
+    delay(100);
+    digitalWrite(amp_sd, LOW);
+    delay(100);
 
-    if (wav[1]->isRunning()) { 
-      // keeps program here until end of viola.h then stops playing
-      if (!wav[1]->loop()) { wav[1]->stop(); stub[1]->stop();} 
-
-      // shuts down amplifier
-      pinMode(14, OUTPUT);
-      delay(100);
-      digitalWrite(14, LOW);
-      delay(100);
-
-      // restarts the esp after x amount of microseconds or 1^-6 seconds
-      ESP.deepSleep(sleep * 1e6);
-    }
+    // restarts the esp after x amount of microseconds or 1^-6 seconds
+    ESP.deepSleep(sleep * 60e6);
   }
 }
